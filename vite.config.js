5355867cfefe5c81c62e3c4a6fa06f6b97572afd
JoachimGautama <joachim.gautama@student.ehb.be>
2026-04-02 20:49:24 +0200

    adjusted base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/frontEnd-sprookje-JoachimGautama/",
+	base: "/",
 });
+
+// base might change to `/frontEnd-sprookje-JoachimGautama/` according to prof
