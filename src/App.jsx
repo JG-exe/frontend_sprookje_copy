4059fa838a2abd9b8ba6b2eef36184da39ca5a9b
JoachimGautama <joachim.gautama@student.ehb.be>
2026-04-10 18:36:41 +0200

    refactor: removed empty index.css
    refactor: moved surviving css file to separate css folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./css/App.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import { BrowserRouter, Routes, Route } from "react-router";
@@ -6,23 +6,20 @@ import Rumpelstiltskin from "./pages/Rumpelstiltskin";
 import Home from "./pages/Home";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter /*basename="/frontEnd-sprookje-JoachimGautama"*/>
-				<Header />
-				<div className="ctx">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route
-							path="/Rumpelstiltskin"
-							element={<Rumpelstiltskin />}
-						/>
-					</Routes>
-				</div>
-			</BrowserRouter>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter /*basename="/frontEnd-sprookje-JoachimGautama"*/>
+        <Header />
+        <div className="ctx">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Rumpelstiltskin" element={<Rumpelstiltskin />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
