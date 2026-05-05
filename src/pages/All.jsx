ded836d8dd85f7157669b06b549279b63851aec4
JoachimGautama <joachim.gautama@student.ehb.be>
2026-05-05 18:53:12 +0200

    feat: recommended now holds a limited amount of books

--- src/pages/All.jsx
+++ src/pages/All.jsx
@@ -1,20 +1,37 @@
+import "../css/components/all.css"
+
 import books from "/books.json";
 import Book from "../components/Book.jsx";
+import {useMemo} from "react";
 
 function All() {
-  return (
-    <div className="centerCt">
-      <div className="recommended books">
-        <p>recommended</p>
-      </div>
-      <div className="bookList">
-        {books.map((book) => (
-          <Book key={book.id} book={book} />
-        ))}
-      </div>
-      <p>This holds all tales</p>
-    </div>
-  );
+    const recommendedBooks = useMemo(() => {
+        const indexes = new Set();
+        while (indexes.size < 3) {
+            indexes.add(Math.floor(Math.random() * books.length));
+        }
+        return [...indexes].map((index) => books[index]);
+    }, [])
+    return (
+        <div className="centerCt">
+            <div className="recommended">
+                <h2>recommended</h2>
+                <div className="bookList">
+                    {recommendedBooks.map((book) => (
+                        <Book key={`rec-${book.id}`} book={book}/>
+                    ))}
+                </div>
+            </div>
+            <br/>
+            <div className="allBooks">
+                <h2>All books</h2>
+                <div className="bookList">
+                    {books.map((book) => (
+                        <Book key={book.id} book={book}/>
+                    ))}</div>
+            </div>
+        </div>
+    );
 }
 
 export default All;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import books from "../../books.json";
 import Book from "../components/Book.jsx";
+import {useMemo} from "react";
 
 function Home() {
+    const recommendedBooks = useMemo( ()=>{
+        const indexes = new Set();
+        while (indexes.size < 3) {
+            indexes.add(Math.floor(Math.random() * books.length));
+        }
+        return [...indexes].map((index) => books[index]);
+    }, [])
 
     return (
         <>
@@ -9,7 +17,7 @@ function Home() {
                 <div className="centerCt">
                     <h2>In the spotlight</h2>
                     <div className={"bookList"}>
-                        {books.map((book) => (
+                        {recommendedBooks.map((book) => (
                             <Book key={book.id} book={book}/>
                         ))}
                     </div>
