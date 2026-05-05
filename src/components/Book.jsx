1629799d88779d22309c849b92704dcf196cd2de
JoachimGautama <joachim.gautama@student.ehb.be>
2026-05-06 01:34:28 +0200

    style: styled the button and link inside the book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,112 +1,114 @@
-import { useState, useRef, useEffect } from "react";
-import { createPortal } from "react-dom";
+import {useState, useRef, useEffect} from "react";
+import {createPortal} from "react-dom";
 import "../css/components/book.css";
 
-function Book({ book }) {
-  const [status, setStatus] = useState("idle");
-  const [originRect, setOriginRect] = useState(null);
-  const slotRef = useRef(null);
-  const bookRef = useRef(null);
+function Book({book}) {
+    const [status, setStatus] = useState("idle");
+    const [originRect, setOriginRect] = useState(null);
+    const slotRef = useRef(null);
+    const bookRef = useRef(null);
 
-  const handleClick = () => {
-    if (status !== "idle") return;
-    const rect = slotRef.current.getBoundingClientRect();
-    setOriginRect(rect);
-    setStatus("opening");
-  };
+    const handleClick = () => {
+        if (status !== "idle") return;
+        const rect = slotRef.current.getBoundingClientRect();
+        setOriginRect(rect);
+        setStatus("opening");
+    };
 
-  useEffect(() => {
-    const current = bookRef.current;
-    const style = current.style;
-    if (status === "opening" && bookRef.current && originRect) {
-      style.top = `${originRect.top}px`;
-      style.left = `${originRect.left}px`;
-      style.width = `${originRect.width}px`;
-      style.transform = `none`;
+    useEffect(() => {
+        const current = bookRef.current;
+        const style = current.style;
+        if (status === "opening" && bookRef.current && originRect) {
+            style.top = `${originRect.top}px`;
+            style.left = `${originRect.left}px`;
+            style.width = `${originRect.width}px`;
+            style.transform = `none`;
 
-      requestAnimationFrame(() => {
-        style.top = "";
-        style.left = "";
-        style.width = "";
-        style.transform = "";
-      });
-    }
-  });
-
-  const handleClose = (e) => {
-    e.stopPropagation();
-    console.log("click");
-    const current = bookRef.current;
-    const style = current.style;
-
-    const rect = slotRef.current.getBoundingClientRect();
-
-    style.top = `${rect.top}px`;
-    style.left = `${rect.left}px`;
-    style.width = `${rect.width}px`;
-    style.transform = `none`;
-
-    requestAnimationFrame(() => {
-      style.top = `${originRect.top}px`;
-      style.left = `${originRect.left}px`;
-      style.width = `${originRect.width}px`;
-      setStatus("closing");
+            requestAnimationFrame(() => {
+                style.top = "";
+                style.left = "";
+                style.width = "";
+                style.transform = "";
+            });
+        }
     });
-  };
 
-  const handleTransitionEnd = (e) => {
-    if (e.propertyName !== "width") return;
-    if (status === "opening") setStatus("open");
-    if (status === "closing") {
-      bookRef.current.style.cssText = "";
-      setStatus("idle");
-    }
-  };
+    const handleClose = (e) => {
+        e.stopPropagation();
+        console.log("click");
+        const current = bookRef.current;
+        const style = current.style;
 
-  const bookMarkup = (
-    <div
-      ref={bookRef}
-      className={`animate book ${status}`}
-      id={`book${book.id}`}
-      onClick={handleClick}
-      onTransitionEnd={handleTransitionEnd}
-    >
-      <div className="animate cover">
-        <div className="front">
-          <img src={book.cover} alt="" />
-          <div className="info animate">
-            <div className="story">{book.title}</div>
-            <div className="creator">{book.author}</div>
-            <div className="creator">{book.creator}</div>
-          </div>
-        </div>
+        const rect = slotRef.current.getBoundingClientRect();
 
-        <div className="back">
-          <div className="pagectx">
-            <p>Created for Front-End MCT 2</p>
-          </div>
-        </div>
-      </div>
-      <div className="inner">
-        <div className="right half">
-          <div className="pagectx">
-            <div className="info">
-              <div className="story">{book.title}</div>
-              <div className="creator">{book.author}</div>
-              <div className="creator">{book.creator}</div>
+        style.top = `${rect.top}px`;
+        style.left = `${rect.left}px`;
+        style.width = `${rect.width}px`;
+        style.transform = `none`;
+
+        requestAnimationFrame(() => {
+            style.top = `${originRect.top}px`;
+            style.left = `${originRect.left}px`;
+            style.width = `${originRect.width}px`;
+            setStatus("closing");
+        });
+    };
+
+    const handleTransitionEnd = (e) => {
+        if (e.propertyName !== "width") return;
+        if (status === "opening") setStatus("open");
+        if (status === "closing") {
+            bookRef.current.style.cssText = "";
+            setStatus("idle");
+        }
+    };
+
+    const bookMarkup = (
+        <div
+            ref={bookRef}
+            className={`animate book ${status}`}
+            id={`book${book.id}`}
+            onClick={handleClick}
+            onTransitionEnd={handleTransitionEnd}
+        >
+            <div className="animate cover">
+                <div className="front">
+                    <img src={book.cover} alt=""/>
+                    <div className="info animate">
+                        <div className="story">{book.title}</div>
+                        <div className="creator">{book.author}</div>
+                        <div className="creator">{book.creator}</div>
+                    </div>
+                </div>
+
+                <div className="back">
+                    <div className="pagectx">
+                        <p>Created for Front-End MCT 2</p>
+                    </div>
+                </div>
             </div>
-            <button>go to story</button>
-            {status === "open" && <button onClick={handleClose}>close</button>}
-          </div>
+            <div className="inner">
+                <div className="right half">
+                    <div className="pagectx">
+                        <div className="info">
+                            <div className="story">{book.title}</div>
+                            <div className="creator">{book.author}</div>
+                            <div className="creator">{book.creator}</div>
+                        <div className={"actions"}>
+                            <a href={"#"} className="linkBtn">go to story</a><br/>
+                            {status === "open" && <button onClick={handleClose}>close</button>}
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-  return (
-    <div className="bookSlot" ref={slotRef}>
-      {status === "idle" ? bookMarkup : createPortal(bookMarkup, document.body)}
-    </div>
-  );
+    );
+    return (
+        <div className="bookSlot" ref={slotRef}>
+            {status === "idle" ? bookMarkup : createPortal(bookMarkup, document.body)}
+        </div>
+    );
 }
 
 export default Book;
