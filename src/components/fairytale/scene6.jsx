3f52cccd55a93a7b3b248956ee2e2ab8a0b2e8ae
JoachimGautama <joachim.gautama@student.ehb.be>
2026-05-10 22:29:55 +0200

    feat: finished scene 6

--- src/components/fairytale/scene6.jsx
+++ src/components/fairytale/scene6.jsx
@@ -1,12 +1,15 @@
 import {motion} from "motion/react";
 
-function Scene6({ s, center }) {
+function Scene6({s, center}) {
     const m = motion
     return (
         <>
             <m.div className={"parallax-lock"} style={{opacity: s.sceneFade}}>
-                <m.p className={"parallax-lock txt"} style={{opacity: s.txtOpacity1, ...center, top: "60vh"}}>
-                    The king demanded the girl show up at the palace.
+                <m.p className={"parallax-lock txt"} style={{...center, top: "50vh"}}>
+                    The king demanded the girl show up at the palace.<br/>
+                    <m.span style={{opacity: s.txtOpacity1}}>
+                        The daughter, scared to refuse, showed up soon after.
+                    </m.span>
                 </m.p>
             </m.div>
         </>)
--- src/hooks/fairytale/useScene6.js
+++ src/hooks/fairytale/useScene6.js
@@ -1,15 +1,13 @@
 import {useTransform} from "motion/react";
 
 export const useScene6 = (scrollY, prevSceneEnd) => {
-    const sceneEnd = prevSceneEnd + 1000;
-    const sceneFade = useTransform(scrollY, [prevSceneEnd + 50, prevSceneEnd + 90, sceneEnd - 100, sceneEnd], [0, 1, 1, 0]);
-    const txtOpacity1 = useTransform(scrollY, [prevSceneEnd + 10, prevSceneEnd + 50], [1, 1])
-    const txtOpacity2 = useTransform(scrollY, [prevSceneEnd + 10, prevSceneEnd + 50], [0, 1])
+    const sceneEnd = prevSceneEnd + 500;
+    const sceneFade = useTransform(scrollY, [prevSceneEnd + 40, prevSceneEnd + 90, sceneEnd - 100, sceneEnd], [0, 1, 1, 0]);
+    const txtOpacity1 = useTransform(scrollY, [prevSceneEnd + 250, prevSceneEnd + 280], [0, 1])
 
     return {
         sceneEnd,
         sceneFade,
         txtOpacity1,
-        txtOpacity2
     }
 }
