e685c7f770ae5a35e5c336aada3d74e17dcd4566
JoachimGautama <joachim.gautama@student.ehb.be>
2026-05-26 17:47:24 +0200

    update base link

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === 'build' ? '/frontEnd-sprookje-JoachimGautama/' : '/',
+    base: command === 'build' ? '/frontend_sprookje_copy/' : '/',
     server: {
       port: 4173,
     },
   };
 });
-
-// base might change to `/frontEnd-sprookje-JoachimGautama/` according to prof
